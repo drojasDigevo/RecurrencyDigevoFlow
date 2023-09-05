96d0f4702443883b840d205136106ec7d7659748
Favio Náquira <fnaquiravargas@gmail.com>
2023-09-05 10:46:05 -0500

    [FIX] Cancelar

--- src/api/subscriptions.js
+++ src/api/subscriptions.js
@@ -44,12 +44,12 @@ exports.subscriptionAPIRenewal = async function (idSubscription) {
 exports.subscriptionAPILayOff = async function (idSubscription) {
     try {
         const response = await InstanceAPI.post(
-            '/subscription/cancel?code=FtlIXQZ64Dbl7rcuGrvI8DHemNlkZcjd0c9TpdmsVHgBAzFuFR2hHw==',
+            '/subscription/lay_off?code=FtlIXQZ64Dbl7rcuGrvI8DHemNlkZcjd0c9TpdmsVHgBAzFuFR2hHw==',
             {
                 idSubscription: idSubscription,
-                idReason: 1,
-                subReason: 1,
-                description: "Cancelado por incumplimiento en el pago"
+                idReason: 2,
+                subReason: 6,
+                description: "Suspendido por razones varias"
             })
         if (response?.data) {
             const { statusCode, content } = response.data
