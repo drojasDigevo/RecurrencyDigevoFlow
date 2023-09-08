a5ec86437cb6815a82da43c8b0bdd6152f7df14d
Favio Náquira <fnaquiravargas@gmail.com>
2023-09-08 13:47:31 -0500

    [FIX] source del pago es nuevamente nativa, no forzada

--- src/api/payments.js
+++ src/api/payments.js
@@ -1,43 +1,45 @@
-const axios = require('axios')
-const moment = require("moment-timezone")
-const InstanceAPI = require("../utils/axios")
+const axios = require("axios");
+const moment = require("moment-timezone");
+const InstanceAPI = require("../utils/axios");
 
-exports.paymentAPICollect = async function (transbankUser,amount,idSubscription) {
-    try {
-        //return false // TEST: para probar limite de intentos de cobro
-        const {data:{token}} = await axios.post(`https://api.digevopayments.com/api/login`,
-		{
-			"email": "admin@ryk.cl",
-			"password": "[KqT7J]LH!q_]U)T"
-		})
-        const {data} = await axios.post(`https://api.digevopayments.com/api/jwt/oneclick-mall/pago/${transbankUser}`,
+exports.paymentAPICollect = async function (transbankUser, amount, idSubscription) {
+	try {
+		//return false // TEST: para probar limite de intentos de cobro
+		const {
+			data: { token },
+		} = await axios.post(`https://api.digevopayments.com/api/login`, {
+			email: "admin@ryk.cl",
+			password: "[KqT7J]LH!q_]U)T",
+		});
+		const { data } = await axios.post(
+			`https://api.digevopayments.com/api/jwt/oneclick-mall/pago/${transbankUser}`,
 			{
-				"codExternal": idSubscription,
-				"amount": amount,
-				"installments": "1",
-				"urlOK": "",
-				"urlError": "",
-				"urlNotify": ""
+				codExternal: idSubscription,
+				amount: amount,
+				installments: "1",
+				urlOK: "",
+				urlError: "",
+				urlNotify: "",
 			},
 			{
-				headers: {Authorization:`Bearer ${token}`}
+				headers: { Authorization: `Bearer ${token}` },
 			}
-		)
-        return data
-    } catch (error) {
-        throw error
-    }
-}
+		);
+		return data;
+	} catch (error) {
+		throw error;
+	}
+};
 
 exports.paymentAPINotify = async function (idSubscription, payment) {
-    try {
-        const response = await InstanceAPI.post(
-            '/subscription/make_payment?code=FtlIXQZ64Dbl7rcuGrvI8DHemNlkZcjd0c9TpdmsVHgBAzFuFR2hHw==',
-            {
-                idSubscription,
-                payment:{
-					payStatus: 'approved',
-					statusDetail: 'Approved detail',
+	try {
+		const response = await InstanceAPI.post(
+			"/subscription/make_payment?code=FtlIXQZ64Dbl7rcuGrvI8DHemNlkZcjd0c9TpdmsVHgBAzFuFR2hHw==",
+			{
+				idSubscription,
+				payment: {
+					payStatus: "approved",
+					statusDetail: "Approved detail",
 					idUserExternal: payment.id_user_external,
 					authCode: payment.auth_code,
 					cardType: payment.card_type,
@@ -45,22 +47,23 @@ exports.paymentAPINotify = async function (idSubscription, payment) {
 					expYear: "",
 					lastFour: payment.last_four,
 					cardCategory: "",
-					paymentType: 'Transbank',
+					paymentType: payment.source,
 					commerceCode: payment.commerce_code,
 					gatewayToken: payment.transbank_user,
 					payDate: payment.payment_date,
 					installments: payment.installments,
 					amount: payment.amount,
-				}
-            },
-			{headers:{'Authorization': 'Bearer ADJKDFJKJF52554FKJDKJKIF---**FJHDJHJDHJHDJHDKLF5'}})
+				},
+			},
+			{ headers: { Authorization: "Bearer ADJKDFJKJF52554FKJDKJKIF---**FJHDJHJDHJHDJHDKLF5" } }
+		);
 
-        if (response?.data) {
-            const { statusCode, content } = response.data
-            if (statusCode === 200) return content
-        }
-        throw new Error("Error API /subscription/make_payment");
-    } catch (error) {
-        throw error
-    }
-}
+		if (response?.data) {
+			const { statusCode, content } = response.data;
+			if (statusCode === 200) return content;
+		}
+		throw new Error("Error API /subscription/make_payment");
+	} catch (error) {
+		throw error;
+	}
+};
